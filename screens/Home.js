7365a64717fdb27ae28c8a5764da2eff2ec6a890
Morten Spjøtvoll <mortents95@gmail.com>
2021-05-22 13:51:25 +0200

    Added pdf reader

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Text } from "react-native";
+import PDFReader from "rn-pdf-reader-js";
 
 class Home extends React.Component {
   render() {
-    return <Text></Text>;
+    return (
+      <PDFReader
+        source={{
+          uri: "http://www.africau.edu/images/default/sample.pdf",
+        }}
+      />
+    );
   }
 }
 export default Home;
