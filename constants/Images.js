9fcae996b92dd0144fdc1186672b84a57856d7a8
personal <mortents95@gmail.com>
2021-06-07 18:01:52 +0200

    build version

--- constants/Images.js
+++ constants/Images.js
@@ -1,6 +1,7 @@
 const Logo = require("../assets/logo-liggende.jpg");
 const FlagNO = require("../assets/flag-no.png");
 const FlagUS = require("../assets/flag-us.png");
+const overviewPDF = require("../assets/melhuskulturen.pdf");
 
 export default {
   Logo,
--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
 import PDFReader from "rn-pdf-reader-js";
-import InstagramEmbed from "react-instagram-embed";
 
 class Home extends React.Component {
   render() {
+
     return (
       <PDFReader
         source={{
-          uri: "http://www.africau.edu/images/default/sample.pdf",
+          uri: "https://www.keepandshare.com/doc30/112022/kalender-versjon-2-pdf-162k?dn=y&dnad=y",
         }}
       />
     );
